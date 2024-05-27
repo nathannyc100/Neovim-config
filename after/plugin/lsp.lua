8ef6a75667943a961c08fc2b395f4711801ea1fe
nathannyc100 <107283236+nathannyc100@users.noreply.github.com>
2024-05-27 18:25:17 +0800

    feat: added lsp commands (not finished)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,9 +1,9 @@
-local lsp_zero = require('lsp-zero')
+local lsp = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
+lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({buffer = bufnr})
 end)
 
 -- to learn how to use mason.nvim with lsp-zero
@@ -12,7 +12,29 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = { "tailwindcss", "tsserver", "eslint", "lua_ls" },
   handlers = {
-    lsp_zero.default_setup,
+    lsp.default_setup,
   },
 })
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+    
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+lsp.setup()
+
 
