05595cb9bc1be9d4ab32f5e30db0cdfd6f6a3bb2
Nathan Yu <nathannycnathannyc@gmail.com>
2024-07-03 22:21:33 +0800

    fix: lsp working

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,18 +16,28 @@ require('mason-lspconfig').setup({
   },
 })
 
+
+lsp.preset('recommended')
+
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
+
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    })
+
 })
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
 
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
