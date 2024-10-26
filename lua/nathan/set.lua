eff633c84b99c977de43d3c46ffa28d3400f52a8
Nathan Yu <nathanyu@Nathans-MacBook-Pro.local>
2024-10-26 17:06:08 -0400

    feat: js 2 space indent

--- lua/nathan/set.lua
+++ lua/nathan/set.lua
@@ -26,3 +26,13 @@ vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "javascript",
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
+
